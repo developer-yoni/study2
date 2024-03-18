57fb00e645eefc3f66442f756cd9b718f728c258
yoni <kkt1692@konkuk.ac.kr>
2024-03-18 23:27:37 +0900

    [feat] : 멀티쓰레드로 요청시, 레이스컨디션 문제로 인해 한정수량보다 더 많은 쿠폰이 생성되는 테스트 수행

--- api/src/main/java/com/example/api/domain/coupon/service/CouponService.java
+++ api/src/main/java/com/example/api/domain/coupon/service/CouponService.java
@@ -18,6 +18,10 @@ public class CouponService {
     @Transactional
     public void applyCoupon(Long userId) {
 
+        /**
+         * [레이스 컨디션 발생]
+         * coupon 개수를 읽어와서 -> insert 하기까지의 section이 critical section이 아니므로
+         *                   -> 읽어온 coupon 개수가 아직 (insert 예정이지만) insert 되기 전 값을 읽어오면 문제 발생 */
         //1. coupone 개수 조회 후
         Long couponCount = couponRepository.countByEntityStatus(EntityStatus.ACTIVE);
 
@@ -27,6 +31,9 @@ public class CouponService {
             return;
         }
 
+        System.out.println("*******************");
+        System.out.println("Thread Name : " + Thread.currentThread().getName() + " / 읽어온 coupon 개수 : " + couponCount);
+        System.out.println("*******************");
         //2_2. 100개 미만이면 발급
         Coupon coupon = Coupon.create(userId);
         couponRepository.save(coupon);
--- api/src/test/java/com/example/api/domain/coupon/service/CouponServiceTest.java
+++ api/src/test/java/com/example/api/domain/coupon/service/CouponServiceTest.java
@@ -5,6 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import com.example.api.domain.coupon.repository.CouponRepository;
 import com.example.api.global.enums.EntityStatus;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -39,5 +42,39 @@ class CouponServiceTest {
         //then
         Long couponCount = couponRepository.countByEntityStatus(EntityStatus.ACTIVE);
         assertThat(couponCount).isEqualTo(1L);
+    }
+
+    @Test
+    @DisplayName("100개보다 더 많은 1000개의 요청이 동시에 왔을때, 한정된 100개의 쿠폰만 생성되는것에 실패")
+    void _100개동시요청에대해_100개의한정수량_쿠폰생성에_실패() throws Exception {
+
+        //given
+        // ExecutorService는 멀티쓰레드 동장을 간단하게 도와주는 Java 모듈
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+        //when
+        for (int i = 0; i < threadCount; i++) {
+
+            int finalI = i;
+            executorService.submit(() -> {
+
+                /** main tread에 의해 비동기로 호출되는 각 worker thread가 couponService.applyCoupon()을 호출한 후, finally 문에서 countDown() 호출한다 */
+                try {
+
+                    couponService.applyCoupon(Long.valueOf(finalI));
+                } finally {
+
+                    countDownLatch.countDown();
+                }
+            });
+        }
+        /** 비동기로 각 worker thread에게 작업을 위임한 후, 여기로 내려온 main thread가 , countDownLatch의 countDown이 0이 될때까지, main thread가 대기한다 */
+        countDownLatch.await();
+
+        //then
+        Long couponCount = couponRepository.countByEntityStatus(EntityStatus.ACTIVE);
+        assertThat(couponCount).isEqualTo(100L);
     }
 }
